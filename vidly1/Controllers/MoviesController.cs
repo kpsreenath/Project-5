abfcb8b78c0a3297abcba1df6668ddba198cf7f2
kpsreenath <48932907+kpsreenath@users.noreply.github.com>
2019-08-27 13:13:52 +0530

    Add files via upload

---
+++ vidly1/Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vidly.Models;
+
+namespace vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: /Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shreak!" };
+            //return View(movie);
+            //return Content("Hello World!");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            return RedirectToAction("index", "Home", new{page=1, SortBy = "name" });
+        }
+	}
+}
